a9bc3616e2a1355b941310c0199f266c77a735df
XavierLin <xavier.lin.lz@outlook.com>
2026-09-09 23:56:53 +0800

    day 3

--- signal_station.py
+++ signal_station.py
@@ -1,41 +1,34 @@
 from os import read
 import sys
+import math
 
 n, m = map(int, input().split())
 
 loc = sorted(list(map(int, input().split())))
+loc.append(math.inf)
 
-i, j = 0, 0
 pre = loc[0]
 res = 0
-ready = False
-k = 0
+
+k = 1
 while k < n:
-    while loc[k] - pre < m:
-        k += 1
-        if k >= n:
-            res += 1
-            break
-    if k >= n:
-        # res += 1
-        break
-    pre = loc[k]
-    res += 1
-    k += 1
-    if k >= n:
-        # res += 1
-        break
     while loc[k] - pre <= m:
         k += 1
-        if k >= n:
-            res += 1
-            break
+
+    pre = loc[k-1]
+    res += 1
+    # k += 1r
     if k >= n:
-        # res += 1
         break
+
+    while loc[k] - pre <= m:
+        k += 1
+
     pre = loc[k]
 # if loc[n-1] - pre > m:
 #     res += 1
+if n == 1:
+    res = 1
 print(res)
 
 
